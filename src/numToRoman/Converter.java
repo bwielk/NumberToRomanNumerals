1f6b81622026f8e53c9d1d32dca85573119662b7
bwielk <bwielk@gmail.com>
2017-12-17 02:19:37 +0100

    Simple teen-numbers are processed

--- src/numToRoman/Converter.java
+++ src/numToRoman/Converter.java
@@ -4,30 +4,21 @@ import java.util.HashMap;
 
 public class Converter {
 	
-	private static final HashMap<Integer, String> tableValue = new HashMap<Integer, String>(){{
-		put(1,"I");
-		put(4,"IV");
-		put(5,"V");
-		put(9,"IX");
-		put(10,"X");
-		put(40,"XL");
-		put(50,"L");
-		put(90,"XC");
-		put(100,"C");
-		put(400,"CD");
-		put(500,"D");
-		put(900,"CM");
-		put(1000,"M");
-	}};
-
 	public String run(int num) {
-		return "";
-	}
-	
-	public String getRoman(int num){
-		if(tableValue.containsKey((Integer)num)){
-			return tableValue.get((Integer)num);
+		
+		int[]numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+		String[]romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		int number = num;
+		String result = "";	
+		while(number != 0){
+			for(int i = 0; i<numbers.length; i++){
+				if(numbers[i]<=number){
+					result = result + romanNumerals[i];
+					number -= numbers[i];
+				}
+			}
 		}
-		return "The key doesn't exist";
+		return result;
 	}
+
 }
--- tests/numToRoman/ConverterTest.java
+++ tests/numToRoman/ConverterTest.java
@@ -15,18 +15,13 @@ public class ConverterTest {
 	}
 	
 	@Test
-	public void romanUnitsCanBeGenerated(){
-		assertEquals("I", converter.getRoman(1));
-		assertEquals("X", converter.getRoman(10));
-		assertEquals("XL", converter.getRoman(40));
-		assertEquals("L", converter.getRoman(50));
-		assertEquals("C", converter.getRoman(100));
-	}
-	
-	@Test
-	public void romanUnitsCannotBeGenerated(){
-		assertEquals("The key doesn't exist", converter.getRoman(12));
-		assertEquals("The key doesn't exist", converter.getRoman(42));
-		assertEquals("The key doesn't exist", converter.getRoman(2));
+	public void turnsTeensIntoRomans(){
+		assertEquals("XI", converter.run(11));
+		assertEquals("XIII", converter.run(13));
+		assertEquals("XIV", converter.run(14));
+		assertEquals("XV", converter.run(15));
+		assertEquals("XVI", converter.run(16));
+		assertEquals("XVIII", converter.run(18));
+		assertEquals("XIX", converter.run(19));
 	}
 }
